0d7ae74d216e444ebdf790f94f396881a459dcef
ChegeKenya <chege.kevin@gmail.com>
2026-05-31 11:44:15 +0300

    refactor(encoder): streamline JPEG encoding process

--- crates/lagos-lite/src/encoder.rs
+++ crates/lagos-lite/src/encoder.rs
@@ -15,13 +15,22 @@
 // crates/lagos-lite/src/encoder.rs
 // Author: Kevin Chege, Location: Nairobi, Date: 21st May 2026
 
+use image::ImageEncoder;
 use jpeg_encoder::{Encoder, ColorType};
 
 pub fn compress_rgba_to_jpeg(width: u32, height: u32, rgba_data: &[u8], quality: u8) -> Result<Vec<u8>, String> {
     let mut output = Vec::new();
-    let encoder = Encoder::new(&mut output, quality);
-    encoder.encode(rgba_data, width as u16, height as u16, ColorType::Rgba)
+    Encoder::new(&mut output, quality)
+        .encode(rgba_data, width as u16, height as u16, ColorType::Rgba)
         .map_err(|e| format!("Failed to encode JPEG: {}", e))?;
 
+    Ok(output)
+}
+
+pub fn compress_rgba_to_png(width: u32, height: u32, rgba_data: &[u8]) -> Result<Vec<u8>, String> {
+    let mut output = Vec::new();
+    image::codecs::png::PngEncoder::new(&mut output)
+        .write_image(rgba_data, width, height, image::ColorType::Rgba8)
+        .map_err(|e| format!("Failed to encode PNG: {}", e))?;
     Ok(output)
 }
